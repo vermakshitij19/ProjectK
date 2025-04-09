0473252aa10bc5a5d9788074c85eff18b8e331bd
vermakshitij19 <34025947+vermakshitij19@users.noreply.github.com>
2025-04-10 00:55:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import boto3
+
+# Initialize EC2 client
+ec2 = boto3.client('ec2', region_name='us-east-1')  # Change region as needed
+
+# Instance ID to stop
+instance_id = 'i-0123456789abcdef0'  # Replace with your EC2 instance ID
+
+# Stop the instance
+response = ec2.stop_instances(InstanceIds=[instance_id])
+
+# Print response
+print("Stopping instance:", instance_id)
+print(response)
